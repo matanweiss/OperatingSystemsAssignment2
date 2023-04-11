8ef778d8497c0bda4dbf2d45c6a5c2faaf57ccfe
Matan <Matan1111@gmail.com>
2023-04-11 15:56:03 +0300

    implemented copy

---
+++ copy.c
@@ -0,0 +1,52 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int copy(char *filename1, char *filename2, int verbose, int force)
+{
+    FILE *file1 = fopen(filename1, "rb");
+    if (!force && access(filename2, F_OK) == 0)
+    {
+        if (verbose)
+            printf("target file exist\n");
+        return 1;
+    }
+    FILE *file2 = fopen(filename2, "wb");
+    if (!file1 || !file2)
+    {
+        if (verbose)
+            printf("general failure\n");
+        return 1;
+    }
+    char c = fgetc(file1);
+    while (c != EOF)
+    {
+        putc(c, file2);
+        c = fgetc(file1);
+    }
+    if (verbose)
+        printf("success\n");
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3 || 5 < argc)
+    {
+        printf("usage: ./cmp <file1> <file2> -v\n");
+        return 1;
+    }
+    int force = 0;
+    int verbose = 0;
+    if (3 < argc && !strcmp(argv[3], "-v"))
+        verbose = 1;
+    if (3 < argc && !strcmp(argv[3], "-f"))
+        force = 1;
+    if (4 < argc && !strcmp(argv[4], "-v"))
+        verbose = 1;
+    if (4 < argc && !strcmp(argv[4], "-f"))
+        force = 1;
+    return copy(argv[1], argv[2], verbose, force);
+}
--- makefile
+++ makefile
@@ -1,4 +1,6 @@
 cmp: cmp.c
 	gcc -o cmp cmp.c
+copy: copy.c
+	gcc -o copy copy.c
 clean:
-	rm cmp
+	rm cmp copy
